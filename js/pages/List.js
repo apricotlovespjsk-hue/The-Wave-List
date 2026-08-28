c208ea259cd552d2ed3a2e7c7632c64fae3322eb
apricotlovespjsk-hue <apricotlovespjsk@gmail.com>
2026-08-27 19:25:47 -0700

    Remove Legacy List record acceptance policy
    Removed outdated information regarding Legacy List records.

--- js/pages/List.js
+++ js/pages/List.js
@@ -120,9 +120,7 @@ export default {
                     <p>
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
-                    </p>
+                    <p>            </p>
                 </div>
             </div>
         </main>
